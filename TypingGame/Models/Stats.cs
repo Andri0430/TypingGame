61b6f83740ec8fb8d0351467bd1a089ae4a893ea
andri <andri@LAPTOP-6FVFR528>
2023-01-16 10:53:13 +0700

    Add project files.

---
+++ TypingGame/Models/Stats.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Headers;
+using System.Xml.Schema;
+
+namespace TypingGame.Models
+{
+    public class Stats
+    {
+        public int Correct { get; set; }
+        public int Missed { get; set; }
+        public int Total
+        {
+            get
+            {
+                return Correct+ Missed;
+            }
+
+            set { Correct = value; }    
+        }
+        public int Accuracy { get; set; }
+
+        public void CountAccuracy()
+        {
+            Accuracy = Correct*100 / (Correct + Missed);
+        }
+    }
+
+}
